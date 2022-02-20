8d456036ce58cb19b2d92b7b5e7ac8a2fe98a33f
heyeman <abdisaheyeman083@gmail.com>
2022-02-20 17:54:05 +0300

    final

--- week1/circle.py
+++ week1/circle.py
@@ -7,17 +7,14 @@ class Circle:
 		bigger = Circle(self.radius + circle.radius)
 		bigger.circles.extend([self, circle])
 		return bigger
-
-	
-
 	def perimeter(self):
 		return (22/7)*(self.radius*2)
 	def area(self):
 		return (22/7)*(self.radius**2)
-c1 = Circle(3)
+"""c1 = Circle(3)
 print(c1.radius)
 c2 = Circle(5)
 print(c2.radius)
 c3 = c1 + c2
 
-print(c3.circles)
+print(c3.radius)"""
